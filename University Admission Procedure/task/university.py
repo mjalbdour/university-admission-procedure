47915cf5881ad846b6fb8ce146ed542cf1bdc460
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-30 12:16:56 +0300

    Stage 3/7: Going big

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -1,10 +1,14 @@
 
 
-MSG_ACCEPTED = "Congratulations, you are accepted!"
-MSG_REJECTED = "We regret to inform you that we will not be able to offer you admission."
-THRESHOLD = 60.0
+# MSG_ACCEPTED = "Congratulations, you are accepted!"
+# MSG_REJECTED = "We regret to inform you that we will not be able to offer you admission."
+MSG_SUCCESSFUL_APPLICANTS = "Successful applicants:"
+# THRESHOLD = 60.0
 
-exams = [float(input()) for _ in range(3)]
-avg = sum(exams) / len(exams)
-print(avg)
-print(MSG_ACCEPTED) if avg >= THRESHOLD else print(MSG_REJECTED)
+N = int(input())
+M = int(input())
+students = [input().split() for _ in range(N)]
+students = sorted(students, key=lambda x: (-float(x[2]), x[0], x[1]))
+print(MSG_SUCCESSFUL_APPLICANTS)
+for student in students[:M]:
+    print(student[0], student[1])
