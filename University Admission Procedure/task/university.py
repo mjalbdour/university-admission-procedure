01594f8fd0265362cd8c21a42eeec9052800c389
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-30 11:12:14 +0300

    Stage 2/7: Raising the bar

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -1,6 +1,10 @@
 
 
 MSG_ACCEPTED = "Congratulations, you are accepted!"
+MSG_REJECTED = "We regret to inform you that we will not be able to offer you admission."
+THRESHOLD = 60.0
+
 exams = [float(input()) for _ in range(3)]
-print(sum(exams) / len(exams))
-print(MSG_ACCEPTED)
+avg = sum(exams) / len(exams)
+print(avg)
+print(MSG_ACCEPTED) if avg >= THRESHOLD else print(MSG_REJECTED)
