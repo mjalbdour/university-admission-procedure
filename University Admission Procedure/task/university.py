a0fb25655e27d9e559188b8dbaa8d415b5b710dd
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-07-01 18:33:42 +0300

    Stage 5/7: Special knowledge

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -1,9 +1,15 @@
 
-MSG_SUCCESSFUL_APPLICANTS = "Successful applicants:"
-
-APPLICANTS_FILENAME = "applicant_list.txt"
-# APPLICANTS_FILENAME = "applicants2.txt"
+APPLICANTS_FILENAME = "applicant_list_5.txt"
+# APPLICANTS_FILENAME = "applicants.txt"
 departments = ["Biotech", "Chemistry", "Engineering", "Mathematics", "Physics"]
+
+dep_grade = {
+    "Physics": 2,
+    "Chemistry": 3,
+    "Biotech": 3,
+    "Mathematics": 4,
+    "Engineering": 5,
+}
 
 departments_students = dict.fromkeys(departments, None)
 
@@ -13,7 +19,7 @@ with open(APPLICANTS_FILENAME, 'r') as applicants_file:
     for line in applicants_file:
         students_original.append(line.split())
 
-students_sorted = sorted(students_original, key=lambda x: (-float(x[2]), x[0], x[1]))
+students_sorted = sorted(students_original, key=lambda x: (-float(x[2]), -float(x[3]), -float(x[4]), float(x[5]), x[0], x[1]))
 for i in range(3):
     for dept in departments:
         if not departments_students[dept]:
@@ -21,8 +27,9 @@ for i in range(3):
 
         if len(departments_students[dept]) >= N:
             continue
+        students_sorted = sorted(students_sorted, key=lambda x: (-float(x[dep_grade[dept]]), x[0], x[1]))
         m = N
-        d_list = [x for x in students_sorted if x[3 + i] == dept]
+        d_list = [x for x in students_sorted if x[6 + i] == dept]
         if len(d_list) > N - len(departments_students[dept]):
             m = m - len(departments_students[dept])
         d_list = d_list[:m]
@@ -32,5 +39,5 @@ for i in range(3):
 
 for dept, students in departments_students.items():
     print(f'\n{dept}')
-    for student in sorted(students, key=lambda x: -float(x[2])):
-        print(student[0], student[1], student[2])
+    for student in sorted(students, key=lambda x: (-float(x[dep_grade[dept]]), x[0], x[1])):
+        print(student[0], student[1], float(student[dep_grade[dept]]))
